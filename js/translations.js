da9064e114530fcfa52ef809835f78b5bd9f6eca
JoaoCoquim <joaocoquim@gmail.com>
2024-09-03 13:09:12 +0100

    Changed language selector and updated design

--- js/translations.js
+++ js/translations.js
@@ -1,34 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const checkbox = document.getElementById('language-switcher');
-    let translations = {};
-  
-    // Function to fetch and load translations
-    async function loadTranslations() {
-      try {
-        const response = await fetch('../translations/translations.json');
-        translations = await response.json();
-        updateContent();
-      } catch (error) {
-        console.log(error);
-      }
+  const languageSelector = document.getElementById('language-selector');
+  let translations = {};
+
+  // Function to fetch and load translations
+  async function loadTranslations() {
+    try {
+      const response = await fetch('../translations/translations.json');
+      translations = await response.json();
+      updateContent();
+    } catch (error) {
+      console.log(error);
     }
-  
-    // Function to update content based on selected language
-    function updateContent() {
-      const language = checkbox.checked ? 'en' : 'pt';
-      const elementsToTranslate = document.querySelectorAll('[data-translate]');
-  
-      elementsToTranslate.forEach(element => {
-        const key = element.getAttribute('data-translate');
-        element.textContent = translations[language][key] || '';
-      });
-  
-    }
-  
-    // Initial load and content update
-    loadTranslations();
-  
-    // Event listener for checkbox change
-    checkbox.addEventListener('change', updateContent);
-  });
-  
+  }
+
+  // Function to update content based on selected language
+  function updateContent() {
+    const language = languageSelector.value;
+    const elementsToTranslate = document.querySelectorAll('[data-translate]');
+
+    elementsToTranslate.forEach(element => {
+      const key = element.getAttribute('data-translate');
+      element.textContent = translations[language][key] || '';
+    });
+  }
+
+  // Initial load and content update
+  loadTranslations();
+
+  // Event listener for dropdown change
+  languageSelector.addEventListener('change', updateContent);
+});
